fae90603d1dbd4d734c9c6238384fc8887b3b572
basic-coding <82214035+basic-coding@users.noreply.github.com>
2021-10-21 20:50:03 +0530

    Add files via upload

--- room.js
+++ room.js
@@ -1,3 +1,24 @@
 function logout(){
     window.location="index.html";
 }
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCjyuxf4HSJXX6ZFbLdfrvMZF-JjEXyThM",
+    authDomain: "pagerweb.firebaseapp.com",
+    databaseURL: "https://pagerweb-default-rtdb.firebaseio.com",
+    projectId: "pagerweb",
+    storageBucket: "pagerweb.appspot.com",
+    messagingSenderId: "447979792770",
+    appId: "1:447979792770:web:625dee21273d763732879b",
+    measurementId: "G-44EREZQQ00"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  function addRoom(){
+    user=document.getElementById("input1").value;
+    firebase.database().ref("/").child(user).update({
+        purpose:"adding user"
+    });
+}
